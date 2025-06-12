e77f0d04c469769af1aaf3d52633730de8a15020
marloBarksdale <lyndenflood@gmail.com>
2025-06-12 19:52:52 -0400

    updated comments

--- LyndenFlood_COMP254Lab3/src/Exercise3.java
+++ LyndenFlood_COMP254Lab3/src/Exercise3.java
@@ -6,9 +6,10 @@ import java.util.Scanner;
 
 public class Exercise3 {
 
-    //ArrayList to store the matching files
+    //ArrayList to store absolute paths of the matching files
     private static List<String> absolutePathsOfFilesFound = new ArrayList<>();
 
+    //file name given by the user
     private static String targetFilename;
 
     public static void main(String[] args) {
@@ -34,7 +35,7 @@ public class Exercise3 {
         scanner.close();
     }
 
-    // Recursive method that uses global filename and list
+
     public static void searchFiles(File directory) {
 
         //Return if the file object is not valid
@@ -47,15 +48,16 @@ public class Exercise3 {
         if (files == null) return;
 
 
-
+        //For each file object in the directory, if it is a directory then enter and continue search else check if the current filename contains the search string
         for (File file : files) {
 
-            //For each file in the directory, if it is a directory then enter and continue search else check if the current filename contains the search string
+
             if (file.isDirectory()) {
 
                 searchFiles(file);
-            } else if (file.getName().toLowerCase().contains(targetFilename.toLowerCase())) {
+            } else if (file.getName().toLowerCase().contains(targetFilename.toLowerCase())) { //Will match any filename containing the target string. Not case sensitive
 //                System.out.println(file.getAbsolutePath());
+                //Add the absolute path of the matched file to the list
                 absolutePathsOfFilesFound.add(file.getAbsolutePath());
             }
         }
